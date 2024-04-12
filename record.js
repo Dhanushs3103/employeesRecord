7a244083c4f8a8251cad940bf7d138a9898a591c
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-04-12 12:37:15 +0530

    added

--- record.js
+++ record.js
@@ -1,1 +1,23 @@
 // fill in javascript code here
+let form = document.getElementById("form");
+let employeeName = document.getElementById("name");
+let employeeId = document.getElementById("employeeId");
+let department = document.getElementById("department");
+let experience = document.getElementById("experience");
+let email = document.getElementById("email");
+let mobile  = document.getElementById("mobile");
+
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    
+    
+}
+
+
+form.addEventListener("submit",function(event){
+    
+    handleSubmit(event);
+    
+})
