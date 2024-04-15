9b49f934bb354d22a7f8000761ee5d39bb55a3b0
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-04-15 12:25:00 +0530

    Added the code

--- record.js
+++ record.js
@@ -1,31 +1,65 @@
 // fill in javascript code here
 let form = document.getElementById("form");
 let employeeName = document.getElementById("name");
-let employeeId = document.getElementById("employeeId");
+let employeeId = document.getElementById("employeeID");
 let department = document.getElementById("department");
 let experience = document.getElementById("experience");
 let email = document.getElementById("email");
 let mobile  = document.getElementById("mobile");
+let tbody = document.getElementsByTagName("tbody")[0];
 
 let data = [];
 
-function handleSubmit(e){
+function handleSubmit (e) {
     e.preventDefault();
     
-    let obj = {};
-    obj.employeeName = employeeName.value;
-    obj.employeeId = employeeId.value;
-    obj.department = department.value;
-    obj.experience = experience.value;
-    obj.email = email.value;
-    obj.mobile = mobile.value;
-    console.log(obj)
+    if(employeeName.value == "" && employeeId.value =="" && department.value == "" & experience.value =="" && email.value == "" && mobile.value == "") {
+     alert("Fill the form credentials");
+    }else {
+        console.log("form submitted");
+        let obj = {};
+        obj.employeeName = employeeName.value;
+        obj.employeeId = employeeId.value;
+        obj.department = department.value;
+        obj.experience = experience.value;
+        obj.email = email.value;
+        obj.mobile = mobile.value;
+    
+        data.push(obj);
+        saveDataToLocalStorage();
+        showData();
+    }
+
+   
+    employeeName.value = "";
+    employeeId.value = "";
+    department.value = "";
+    experience.value = "";
+    email.value = "";
+    mobile.value = "";
+}
+
+function saveDataToLocalStorage() {
+    localStorage.setItem("data",JSON.stringify(data));
+}
+
+function showData () {
+    let storedData = JSON.parse(localStorage.getItem("data"));
+    if(storedData) {
+        tbody.innerHTML = "";
+        storedData.forEach((employeeData,i)=>{
+            let tableRow = document.createElement("tr");
+            let empName =  document.createElement("td");
+            empName.textContent = employeeData.employeeName;
+    
+            tableRow.append(empName);
+            tbody.append(tableRow);
+        })
+    }else {
+        tbody.innerHTML = "<h2>No Data</h2>";
+    }
+    // runing a loop around the data
     
 }
 
-
-form.addEventListener("submit",function(event){
-    
-    handleSubmit(event);
-    
-})
+form.addEventListener("submit",handleSubmit)
